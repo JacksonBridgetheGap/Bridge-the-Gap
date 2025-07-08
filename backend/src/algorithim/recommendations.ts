194ee244af914255e201d99329fba27e0249ff6f
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-07-08 14:13:11 -0700

    Data pipeline set up from backend to frontend, also designed basic structure for data parsing and basic filtering

--- backend/src/algorithim/recommendations.ts
+++ backend/src/algorithim/recommendations.ts
@@ -1,22 +1,26 @@
-import {
-  user_parse,
-  create_user_matrix,
-  create_group_matrix,
-} from "../utils/dataUtils";
+import { user_parse, create_group_matrix } from "../utils/dataUtils";
 import pre_filter from "./pre_filter";
 import { PrismaClient } from "@prisma/client";
 import { UserWithGroups } from "../types/types";
-
 const prisma = new PrismaClient();
 
 //Return the top 4 recommended groups based off a content-based filter
 // 1. Get Groups data with tags
 // 2. Get user classifications using user_parse
 // 3. Pre-Filter the data to remove unecessary groups
-// 4. Create Group and User matrices to be used in filtering
-// 5. Perform cosine similarity to determine strength of recommendation
+// 4. Perform cosine similarity to determine strength of recommendation and sort list of recommendations
 export default async function recommendations(user: UserWithGroups | null) {
+  //1.
+  const groups = await prisma.group.findMany({
+    include: { members: true },
+  });
+  const groupMatrix = create_group_matrix(groups);
+
+  //2.
   const userClassifications = user_parse(user);
-  const groups = await prisma.group.findMany();
-  return groups.splice(0, 4);
+
+  //3.
+  const filteredGroups = pre_filter(user, groups);
+
+  return filteredGroups.splice(0, 4);
 }
--- backend/src/utils/dataUtils.ts
+++ backend/src/utils/dataUtils.ts
@@ -1,16 +1,47 @@
 //Return the users classification tags and scores as a dictionary with tags as keys
 // and values as the number of times those tags appear
 import { UserWithGroups } from "../types/types";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+//Create a user classification map to describe the users relationship strength with each tag
 export function user_parse(user: UserWithGroups | null) {
-  const classification: string[] = [];
+  const classification = new Map();
+
+  //If user isn't apart of any group return an empty map
+  // let user of function handle error detection
+  if (!user?.groups) {
+    return classification;
+  }
   //Need to merge data-model updates before we can pull tags from the users groups
-  console.log(classification);
+  for (let i = 0; i < user?.groups.length; i++) {
+    for (let j = 0; j < user.groups[i].tags.length; j++) {
+      let tag = user.groups[i].tags[j];
+      if (classification.has(tag)) {
+        let currentValue = classification.get(tag);
+        classification.set(tag, currentValue + 1);
+      } else {
+        classification.set(tag, 1);
+      }
+    }
+  }
+
   return classification;
 }
 
-//Creates a user data matrix based on tag data
-export function create_user_matrix() {}
-
 //Creates a group data matrix based on tag data
-export function create_group_matrix() {}
+type RecordsType = Awaited<ReturnType<typeof prisma.group.findMany>>;
+export function create_group_matrix(groups: RecordsType) {
+  const group_matrix = new Map();
+
+  groups.forEach((group) => {
+    let group_map = new Map();
+    group.tags.forEach((tag: string) => {
+      group_map.set(tag, 1);
+    });
+    group_matrix.set(group.id, group_map);
+  });
+
+  return group_matrix;
+}
