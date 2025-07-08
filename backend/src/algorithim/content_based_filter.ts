8dd56442152bc648bc18a62b883c2d11a3ba8a4a
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-07-08 15:19:08 -0700

    Finished implementation of recommendation algorithim

--- backend/src/algorithim/content_based_filter.ts
+++ backend/src/algorithim/content_based_filter.ts
@@ -1,6 +1,47 @@
-function cosineSimilarity(a: number[], b: number[]) {}
+import { cosineSimilarity } from "../math/math";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+const TAG_OPTIONS = [
+  "hobbies",
+  "education",
+  "gaming",
+  "food",
+  "health",
+  "travel",
+  "business",
+  "pets",
+  "miscellaneous",
+];
+
+type RecordsType = Awaited<ReturnType<typeof prisma.group.findMany>>;
 export default function content_based_filter(
   userClassificatoins: Map<string, number>,
   groupMatrix: Map<string, Map<string, number>>,
-) {}
+  groups: RecordsType,
+) {
+  //Construct User vector from classifications
+  const userVector = Array.from({ length: 10 }, () => 0);
+  for (let i = 0; i < TAG_OPTIONS.length; i++) {
+    userVector[i] = userClassificatoins.get(TAG_OPTIONS[i]) || 0;
+  }
+
+  //Loop through groups
+  const relationshipStrengths = new Map();
+  groups.forEach((group) => {
+    //Construct group vector
+    const groupVector = Array.from({ length: 10 }, () => 0);
+    for (let i = 0; i < TAG_OPTIONS.length; i++) {
+      groupVector[i] =
+        groupMatrix.get(group.id.toString())?.get(TAG_OPTIONS[i]) || 0;
+    }
+    //Compare vectors using cosine similarity
+    const similarity = cosineSimilarity(userVector, groupVector);
+    relationshipStrengths.set(group.id, similarity);
+  });
+
+  return groups.sort((a, b) => {
+    return relationshipStrengths.get(b.id) - relationshipStrengths.get(a.id);
+  });
+}
