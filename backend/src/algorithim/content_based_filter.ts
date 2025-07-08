f64d5b5887ee87cd568037917133cc0228308ac9
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-07-08 14:52:42 -0700

    Added function stub

---
+++ backend/src/algorithim/content_based_filter.ts
@@ -0,0 +1,6 @@
+function cosineSimilarity(a: number[], b: number[]) {}
+
+export default function content_based_filter(
+  userClassificatoins: Map<string, number>,
+  groupMatrix: Map<string, Map<string, number>>,
+) {}
