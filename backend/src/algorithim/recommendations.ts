09881c7a4905d019688c5e70a72bb3e8542ba9d9
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-07-08 14:15:31 -0700

    Fixs organization of recommendation function

--- backend/src/algorithim/recommendations.ts
+++ backend/src/algorithim/recommendations.ts
@@ -8,19 +8,22 @@ const prisma = new PrismaClient();
 // 1. Get Groups data with tags
 // 2. Get user classifications using user_parse
 // 3. Pre-Filter the data to remove unecessary groups
+// 4. Create group matrix to pass into recommandation algorithim
 // 4. Perform cosine similarity to determine strength of recommendation and sort list of recommendations
 export default async function recommendations(user: UserWithGroups | null) {
   //1.
   const groups = await prisma.group.findMany({
     include: { members: true },
   });
-  const groupMatrix = create_group_matrix(groups);
 
   //2.
   const userClassifications = user_parse(user);
 
   //3.
   const filteredGroups = pre_filter(user, groups);
+
+  //4.
+  const groupMatrix = create_group_matrix(filteredGroups);
 
   return filteredGroups.splice(0, 4);
 }
