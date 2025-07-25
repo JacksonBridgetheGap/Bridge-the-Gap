0ef9245abd0d0fd365a14c71334ad29e72cb78fe
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-07-25 14:38:58 -0700

    Added confirmation to override user events to conflict resolution modal

--- frontend/src/components/ConflictModal.jsx
+++ frontend/src/components/ConflictModal.jsx
@@ -1,7 +1,18 @@
 import ConflictCard from "./ConflictCard.jsx";
 import BridgeTheGapButton from "./BridgeTheGapButton.jsx";
+import { useState } from "react";
 
 export default function ConflictModal({ display, conflicts, confirm, cancel }) {
+  const [confirmedConflicts, setConfirmedConflicts] = useState([]);
+
+  const handleToggle = (id) => {
+    setConfirmedConflicts((prevSelected) =>
+      prevSelected.includes(id)
+        ? prevSelected.filter((conflictId) => conflictId !== id)
+        : [...prevSelected, id],
+    );
+  };
+
   return (
     <div
       className={`${display ? "block" : "hidden"} fixed inset-0 z-50 flex items-center justify-center bg-black/40`}
@@ -14,20 +25,37 @@ export default function ConflictModal({ display, conflicts, confirm, cancel }) {
           conflicts.
         </p>
 
-        <div className="flex flex-col gap-4 p-2">
+        <div className="flex flex-col gap-4 p-4">
           {conflicts.map((conflict) => {
             return (
-              <div className="bg-red-400 rounded-lg p-2 m-2 border border-red-500">
-                <ConflictCard conflict={conflict} />
+              <div>
+                <div className="bg-red-400 rounded-lg p-2 m-2 border border-red-500">
+                  <ConflictCard conflict={conflict} />
+                </div>
+                <div className={"flex flex-row gap-4 justify-center"}>
+                  <p>Ignore Conflict</p>
+                  <input
+                    type="checkbox"
+                    id={conflict.id}
+                    checked={confirmedConflicts.includes(conflict.id)}
+                    onChange={() => handleToggle(conflict.id)}
+                    className={"rounded-md"}
+                  />
+                </div>
               </div>
             );
           })}
         </div>
+        <p className={"text-gray-500"}>Ignore all conflicts to confirm</p>
         <div
           className="flex justify-center gap-4 p-2"
           style={{ width: "100%" }}
         >
-          <BridgeTheGapButton value={"Add anyway"} onClick={confirm} />
+          <BridgeTheGapButton
+            value={"Add anyway"}
+            onClick={confirm}
+            disabled={confirmedConflicts.length !== conflicts.length}
+          />
           <BridgeTheGapButton value={"Cancel"} onClick={cancel} />
         </div>
       </div>
